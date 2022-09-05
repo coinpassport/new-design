bc89772511b7749b868d07c08453bd98d4143880
numtel <ben@latenightsketches.com>
2022-09-04 21:32:53 -0700

    Can click through all verify steps

--- app.js
+++ app.js
@@ -20,3 +20,29 @@ document.querySelectorAll('[data-menu]').forEach(element => {
     });
   }, false);
 });
+
+document.querySelectorAll('[data-step]').forEach(element => {
+  element.addEventListener('click', event => {
+    const step = element.attributes['data-step'].value;
+    const stepEl = document.getElementById(step);
+    const activeMenuLi = document.querySelector('#wizard>menu>li.active');
+    const thisMenuLi = document.querySelector(`#wizard>menu>li[rel="${step}"]`);
+    if(activeMenuLi) activeMenuLi.classList.toggle('active', false);
+    document.querySelector('#wizard>menu').classList.toggle('hidden', !thisMenuLi);
+    thisMenuLi && thisMenuLi.classList.toggle('active', true);
+    document.querySelectorAll('#wizard>main').forEach(el =>
+      el.classList.toggle('hidden', el.id === step ? false : true));
+  }, true);
+});
+
+document.querySelectorAll('[data-toggler]').forEach(element => {
+  element.addEventListener('click', event => {
+    const toggler = element.attributes['data-toggler'].value;
+    const togglerEl =
+      toggler === 'next' ? element.nextElementSibling :
+      toggler === 'prev' ? element.previousElementSibling :
+      document.getElementById(toggler);
+    element.setAttribute('disabled', '');
+    togglerEl.removeAttribute('disabled');
+  }, true);
+});
